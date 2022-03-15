36c8de480b10c0b202484371c07ace078f92e11d
francmUMA <francano368@gmail.com>
2022-03-15 17:46:27 +0100

    ejemplofor.sh

--- Segundo/Cuatri2/SisOperativos/practica1/ejemplofor.sh
+++ Segundo/Cuatri2/SisOperativos/practica1/ejemplofor.sh
@@ -1,10 +1,6 @@
 #! /bin/bash
-for i in `ls $1`
+for i in `ls -R $1`
 do
-    if [ ! -e $1/$i  ]
-    then
-        echo "$i no existe"
-    fi
     if [ -f $1/$i ]
     then 
         echo "$i -> Es un fichero"
