415982fe9dedf980068ef7883df290e8ff4b56da
francmUMA <francano368@gmail.com>
2022-03-16 09:12:23 +0100

    backup2 listo

--- Segundo/Cuatri2/SisOperativos/practica1/backup2.sh
+++ Segundo/Cuatri2/SisOperativos/practica1/backup2.sh
@@ -1,14 +1,18 @@
 #! /bin/bash
-for file in $(ls $1)
+for file_type in $@
 do
-    num_versiones=$(ls *$file | wc -w)
-    if [ $num_versiones -ge 9 ]
-    then
-        echo "$file ha alcanzado el numero maximo de versiones"
-    else
-        fecha=$(date "+%y%m%d")
-        nombre_nuevo="${fecha}_${file}"
-        cp $file $nombre_nuevo
-    fi
+    for file in $(ls $file_type)
+    do
+        num_versiones=$(ls *$file | wc -w)
+        if [ $num_versiones -ge 9 ]
+        then
+            echo "$file ha alcanzado el numero maximo de versiones"
+        else
+            fecha=$(date "+%y%m%d")
+            nombre_nuevo="${fecha}_${file}"
+            cp $file $nombre_nuevo
+        fi
+    done
 done
-shift
+
+
