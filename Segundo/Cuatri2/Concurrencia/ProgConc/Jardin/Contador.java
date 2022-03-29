419b046a495bdf2e3c932585141ac059f7731c5b
francmUMA <francano368@gmail.com>
2022-03-29 20:22:24 +0200

    propuesta jardines

--- Segundo/Cuatri2/Concurrencia/ProgConc/Jardin/Contador.java
+++ Segundo/Cuatri2/Concurrencia/ProgConc/Jardin/Contador.java
@@ -3,6 +3,15 @@ package Jardin;
 public class Contador {
     private volatile int cont;
 
+    /*
+
+    Propuesta 1:
+    Hacer un array con tantos contadores como hebras se quieran crear (un contador por hebra)
+    y al final sumarlo. Para saber el acceso a cada contador, hay que guardar en cada puerta el
+    número que se le ha asignado.
+    
+    */
+
     public Contador(){
         cont = 0;
     }
--- Segundo/Cuatri2/Concurrencia/ProgConc/Jardin/Principal.java
+++ Segundo/Cuatri2/Concurrencia/ProgConc/Jardin/Principal.java
@@ -1,9 +1,13 @@
 package Jardin;
 
 public class Principal {
-    Contador visitantes = new Contador();
-    Thread puerta1 = new Thread(new Puerta(visitantes, 1000));
-    Thread puerta2 = new Thread(new Puerta(visitantes, 1000));
-    puerta1.start();
-    puerta2.start();
+    public static void main(String[] args) throws InterruptedException {
+        Contador visitantes = new Contador();
+        Thread puerta1 = new Thread(new Puerta(visitantes, 1000));
+        Thread puerta2 = new Thread(new Puerta(visitantes, 1000));
+        puerta1.start();
+        puerta2.start();
+        Thread.sleep(10);
+        System.out.println(visitantes.valor());
+    }
 }
