0edffad9ce7cb9b52ef9c62df716ed1b911824ac
francmUMA <francano368@gmail.com>
2022-04-04 10:17:55 +0200

    actualizacion

--- Segundo/Cuatri2/SisOperativos/practica2/punteros/reverseString.c
+++ Segundo/Cuatri2/SisOperativos/practica2/punteros/reverseString.c
@@ -2,7 +2,19 @@
 #include <string.h>
 
 int reverse(char* fuente, char** destino){
-    
+    //Guardo la direccion de memoria del primer caracter para mover el puntero hasta el final y
+    //hacer que el puntero vuelva y se compare para que vuelva al inicio
+    int res = 0;
+    while (*fuente != '\0'){
+        fuente++;
+        res++;
+    }
+    *destino = malloc(res*sizeof(char));
+    for (int i = res; i > 0; i--){
+        destino[res - i] = fuente[res - i]; 
+        fuente--;
+    } 
+    return res;
 }
 
 int main(){
