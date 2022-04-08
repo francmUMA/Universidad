548ce2bc41ed23d9b22fddf2702ebc0458e12b83
francmUMA <francano368@gmail.com>
2022-04-08 18:59:28 +0200

    practica 4 concurrencia

--- Segundo/Cuatri2/Concurrencia/ProgConc/practica4/Nodo.java
+++ Segundo/Cuatri2/Concurrencia/ProgConc/practica4/Nodo.java
@@ -8,6 +8,10 @@ public class Nodo implements Runnable{
         this.lista = lista;
     }
 
+    public List<Integer> getLista(){
+        return lista;
+    }
+
     private void aniade (List<Integer> l, int desde, int hasta){
         for (int i = desde; i < hasta; i++){
             l.add(lista.get(i));
@@ -16,10 +20,48 @@ public class Nodo implements Runnable{
 
     private void mezcla (List<Integer> l1, List<Integer> l2){
         List<Integer> aux = new ArrayList<>();
+        int size_left_l1 = 0;
+        int size_left_l2 = 0;
+        while (size_left_l1 < l1.size() || size_left_l2 < l2.size()){
+            if (size_left_l1 >= l1.size()){
+                aux.add(l2.get(size_left_l2));
+                size_left_l2++;
+            } else if (size_left_l2 >= l2.size()){
+                aux.add(l1.get(size_left_l1));
+                size_left_l1++;
+            } else {
+                if (l1.get(size_left_l1) < l2.get(size_left_l2)){
+                    aux.add(l1.get(size_left_l1));
+                    size_left_l1++;
+                } else {
+                    aux.add(l2.get(size_left_l2));
+                    size_left_l2++;
+                }
+            }
+        }
+        this.lista = aux;
     }
 
     @Override
     public void run() {
-        
+        if (lista.size() > 1){
+            List<Integer> l1 = new ArrayList<>();
+            aniade(l1, 0, lista.size()/2);
+            Nodo n1 = new Nodo(l1);
+            List<Integer> l2 = new ArrayList<>();
+            aniade(l2, lista.size()/2, lista.size());
+            Nodo n2 = new Nodo(l2);
+            Thread t1 = new Thread(n1);
+            Thread t2 = new Thread(n2);
+            t1.start();
+            t2.start();
+            try {
+                t1.join();
+                t2.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            this.mezcla(n1.getLista(),n2.getLista());
+        }
     }
 }
---
+++ Segundo/Cuatri2/Concurrencia/ProgConc/practica4/PrincipalNodo.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class PrincipalNodo {
+    public static void main(String[] args) throws InterruptedException {
+        Random r = new Random();
+
+        //Crear lista de numeros random
+        int size = r.nextInt(20);
+        List<Integer> lista = new ArrayList<>();
+        for (int i = 0; i < size; i++){
+            lista.add(r.nextInt(50));
+        }
+
+        //Creamos un nodo nuevo
+        Nodo n = new Nodo(lista);
+        Thread t = new Thread(n);
+        t.start();
+        t.join();
+
+        //Mostramos la lista
+        System.out.println("La lista de valores sin ordenar es: " + Arrays.toString(lista.toArray()));
+        System.out.println("La lista de valores ordenados es: " + Arrays.toString(n.getLista().toArray()));
+    }
+}
