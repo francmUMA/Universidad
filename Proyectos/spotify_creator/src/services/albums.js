5095d008d67dbc078d4ecdbbd9a4a9aed6a81741
francm9 <francano368@gmail.com>
2022-08-16 10:32:20 +0200

    Createlify añadido

--- Proyectos/spotify_creator/src/services/albums.js
+++
@@ -1,7 +0,0 @@
-import { SPOTIFY as sp, options } from "../.env";
-
-export const getAlbumTracks = async (token, albumID) => {
-    return fetch(sp.baseURI + "/albums/" + albumID + "/tracks?limit=50", options(token))
-      .then((response) => response.json())
-      .then((items) => items.items);
-};
