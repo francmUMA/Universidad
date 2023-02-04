1d71a86fae51c16b07bb9ad35a22b2e0be7f1f52
francmUMA <francano368@gmail.com>
2023-02-04 16:13:31 +0100

    Arreglo

---
+++ .ccls-cache/@home@fran@Universidad/Tercero@STR@seminario@info.c
@@ -0,0 +1,12 @@
+/*
+ * - Poner unsigned delante de un tipo, elimina la parte negativa del rango de ese tipo.  
+ * - Con los sistemas que trabajamos es importante el elegir el tipo de la variable ya que ocupará
+ *   más o menos en memoria.
+ * - El modificador volatile indica que la posicion de memoria puede ser cambiada por otro programa.
+ * - Todas las variables se declaran al principio, incluso la de los bucles.
+ * - Las directivas no ocupan memoria. Además, se pueden usar para definir macros y que tengan variables
+ * - Las cadenas de texto son arrays que terminan en '\0'
+ *
+*/
+
+
