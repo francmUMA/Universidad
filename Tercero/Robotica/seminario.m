7764497d216b887de66e04a7ac8a530900702a7b
francm9 <francano368@gmail.com>
2023-02-13 16:57:12 +0100

    seminario matlab

---
+++ Tercero/Robotica/seminario.m
@@ -0,0 +1,20 @@
+%Ejercicio 5 
+A = [sin(pi/2) cos(pi/2) 0; -cos(pi/2) sin(pi/2) 0; 0 0 1];
+B = A(2:3,2:3);
+
+%Ejercicio 6
+v = [1:10];
+c = v(1:2:end);
+
+%Ejercicio 7
+A = rand(4,3);
+B = A.^2;
+
+%Ejercicio 9
+%plot(v,v,'c*');
+
+%Ejercicio 14
+h = deconv(v,v);
+
+%Ejercicio 15
+y=[1 3 0 2 0 6]; 
