891c099b3b56cb954e68bde2fc760a34cdfbe650
francm <francano368@uma.es>
2023-03-27 21:45:06 +0200

    Buttons device

--- Tercero/DSO/pi_driver/driver.c
+++ Tercero/DSO/pi_driver/driver.c
@@ -4,6 +4,10 @@
 #include <linux/uaccess.h>
 #include <linux/fs.h>
 #include <linux/miscdevice.h>
+#include <linux/interrupt.h>
+#include <linux/wait.h>
+#include <linux/sched.h>
+#include <linux/jiffies.h>
 
 #define DRIVER_AUTHOR "Francisco Javier Cano Moreno"
 #define DRIVER_DESC "Driver de gestión de los GPIO para RaspberryPI"
@@ -15,9 +19,14 @@
 #define LED5 22
 #define LED6 27
 
-#define GPIO_SPEAKER 4
+#define SPEAKER 4
+
+#define BUTTON1 2
+#define BUTTON2 3
 
 static int LED_GPIOS[] = {LED6, LED5, LED4, LED3, LED2, LED1};
+static char pulsaciones[100];
+static int counter_pulsaciones = 0;
 
 static void byte2leds(char ch, int mode){
     int i;
@@ -100,8 +109,8 @@ static ssize_t speaker_write(struct file *filp, const char *buf, size_t count, l
     unsigned char ch;
 	if (copy_from_user(&ch, buf, 1)) return -EFAULT;
     printk(KERN_INFO "Valor recibido: %d\n", (int) ch);
-    if (ch == '0') gpio_set_value(GPIO_SPEAKER, 0);
-    else gpio_set_value(GPIO_SPEAKER, 1);
+    if (ch == '0') gpio_set_value(SPEAKER, 0);
+    else gpio_set_value(SPEAKER, 1);
     return 1;
 }
 
@@ -117,6 +126,59 @@ static struct miscdevice speaker_miscdev = {
 	.mode  = S_IWUGO,
 };
 
+/****************************************************************************/
+/* Buttons driver file operations */
+/****************************************************************************/
+static short int irq_BUTTON1 = 0;
+static short int irq_BUTTON2 = 0;
+
+static void tasklet_handler(unsigned long);
+
+static void allow_int_timer_handler(unsigned long);
+
+DECLARE_TASKLET(tasklet_button, tasklet_handler);
+
+DEFINE_TIMER(allow_int_timer, allow_int_timer_handler);
+
+static irqreturn_t irq_handler(int irq, void *dev_id, struct pt_regs *regs){
+    if (irq == irq_BUTTON1) tasklet_init(&tasklet_button, tasklet_handler, 1);
+    else if (irq == irq_BUTTON2) tasklet_init(&tasklet_button, tasklet_handler, 2);
+    tasklet_schedule(&tasklet_button);
+    return IRQ_HANDLED;
+}
+
+static void tasklet_handler(unsigned long dato){
+    if (dato == 1) pulsaciones[counter] = '1';
+    else pulsaciones[counter] = '2';
+    counter+=1;
+}
+
+static void allow_int_timer_handler(unsigned long dato){
+    ;
+}
+
+static ssize_t buttons_read(struct file *filp, char *buf, size_t count, loff_t *offset){
+    if (*offset > 0) return 0;
+    int len = strlen(pulsaciones);
+    if (copy_to_user(buf, pulsaciones, len)) return -EFAULT;
+    *offset += len;
+    return len;
+}
+
+
+static const struct file_operations buttons_fops = {
+	.read = buttons_read,
+};
+
+
+static struct miscdevice buttons_miscdev = {
+	.minor = MISC_DYNAMIC_MINOR,
+	.name  = "buttons",
+	.fops  = &buttons_fops,
+	.mode  = S_IRUGO,
+};
+
+
 //Register devices
 static int r_dev_config(void){
 	int ret = 0;
@@ -129,6 +191,11 @@ static int r_dev_config(void){
     if (ret < 0){
 		printk(KERN_ERR "ERROR registering speaker\n");
 	} else printk(KERN_NOTICE "Speaker device has been registered -> speaker_miscdev.minor=%d\n", speaker_miscdev.minor);
+    
+    ret = misc_register(&buttons_miscdev);
+    if (ret < 0){
+		printk(KERN_ERR "ERROR registering buttons\n");
+	} else printk(KERN_NOTICE "Buttons device has been registered -> buttons_miscdev.minor=%d\n", buttons_miscdev.minor);
 
     return ret;
 }
@@ -144,20 +211,29 @@ static void r_cleanup(void) {
     gpio_free(LED4);
     gpio_free(LED5);
     gpio_free(LED6);
-    gpio_free(GPIO_SPEAKER);
+    gpio_free(SPEAKER);
+    gpio_free(BUTTON1);
+    gpio_free(BUTTON2);
 
     if (leds_miscdev.this_device) misc_deregister(&leds_miscdev);
     if (speaker_miscdev.this_device) misc_deregister(&speaker_miscdev);
+    if (buttons_miscdev.this_device) misc_deregister(&buttons_miscdev);
+
+    if (irq_BUTTON1) free(irq_BUTTON1, "Se ha pulsado el boton 1");
+    if (irq_BUTTON2) free(irq_BUTTON2, "Se ha pulsado el boton 2");
+
+    del_timer(&allow_int_timer);
+    tasklet_kill(&tasklet_button);
 
     printk(KERN_NOTICE "Removing %s module\n",KBUILD_MODNAME);
     return;
 }
 
 static int r_init(void) {
-        if (r_dev_config() < 0){
-                r_cleanup();
-                return -1;
-        }       
+    if (r_dev_config() < 0){
+        r_cleanup();
+        return -1;
+    }       
 
     //Request all GPIOs
     if(gpio_is_valid(LED1) < 0) return -1;
@@ -178,9 +254,13 @@ static int r_init(void) {
     if(gpio_is_valid(LED6) < 0) return -1;
         if(gpio_request(LED6, "LED6") < 0) return -1;
 
-    if(gpio_is_valid(GPIO_SPEAKER) < 0) return -1;
-        if(gpio_request(GPIO_SPEAKER, "GPIO_SPEAKER") < 0) return -1;
-    
+    if(gpio_is_valid(SPEAKER) < 0) return -1;
+        if(gpio_request(SPEAKER, "SPEAKER") < 0) return -1;
+    if(gpio_is_valid(BUTTON1) < 0) return -1;
+        if(gpio_request(BUTTON1, "BUTTON1") < 0) return -1;
+    if(gpio_is_valid(BUTTON2) < 0) return -1;
+        if(gpio_request(BUTTON2, "BUTTON2") < 0) return -1;  
+
     //Set GPIO as output
     gpio_direction_output(LED1, 0 );
     gpio_direction_output(LED2, 0 );
@@ -189,7 +269,37 @@ static int r_init(void) {
     gpio_direction_output(LED5, 0 );
     gpio_direction_output(LED6, 0 );
     gpio_direction_output(LED6, 0 );
-    gpio_direction_output(GPIO_SPEAKER, 0 );
+    gpio_direction_output(SPEAKER, 0 );
+
+    //Configure irqs
+    if ((irq_BUTTON1 = gpio_to_irq(BUTTON1)) < 0){
+        printk(KERN_ERR "GPIO to irq mapping failed.\n");
+        return irq_BUTTON1;
+    }
+
+    if ((irq_BUTTON2 = gpio_to_irq(BUTTON2)) < 0){
+        printk(KERN_ERR "GPIO to irq mapping failed.\n");
+        return irq_BUTTON2;
+    }
+
+    int res = 0;
+    if ((res = request_irq(irq_BUTTON1,
+                           (irq_handler_t) irq_handler,
+                           IRQF_TRIGGER_FALLING,
+                           "BUTTON1",
+                           "Se ha pulsado el boton 1"))) {
+        printk(KERN_ERR "IRQ request failed\n")
+        return res;
+    }
+    if ((res = request_irq(irq_BUTTON2,
+                           (irq_handler_t) irq_handler,
+                           IRQF_TRIGGER_FALLING,
+                           "BUTTON2",
+                           "Se ha pulsado el boton 2"))) {
+        printk(KERN_ERR "IRQ request failed\n")
+        return res;
+    }
+
 
     printk(KERN_NOTICE "Hey, %s module is being loaded!\n",KBUILD_MODNAME);
     return 0;
