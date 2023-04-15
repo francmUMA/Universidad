926a45783a37f5b75f4e51a049a954007fb4dfef
francm9 <francano368@gmail.com>
2023-04-15 19:17:00 +0200

    actualizacion

--- Tercero/DIR/GIC-DIR-P1-e6/data-center.sh
+++ Tercero/DIR/GIC-DIR-P1-e6/data-center.sh
@@ -67,32 +67,32 @@ exit
 
 interface FastEthernet 0/0.501
 encapsulation dot1Q 501
-ip address 10.1.0.1 255.255.254.0
+ip address 10.3.0.1 255.255.254.0
 exit
 
 interface FastEthernet 0/0.502
 encapsulation dot1Q 502
-ip address 10.1.2.1 255.255.254.0
+ip address 10.3.2.1 255.255.254.0
 exit
 
 interface FastEthernet 0/0.503
 encapsulation dot1Q 503
-ip address 10.1.4.1 255.255.254.0
+ip address 10.3.4.1 255.255.254.0
 exit
 
 interface FastEthernet 0/0.504
 encapsulation dot1Q 504
-ip address 10.1.6.1 255.255.254.0
+ip address 10.3.6.1 255.255.254.0
 exit
 
 interface FastEthernet 0/0.505
 encapsulation dot1Q 505
-ip address 10.1.8.1 255.255.254.0
+ip address 10.3.8.1 255.255.254.0
 exit
 
 interface FastEthernet 0/0.506
 encapsulation dot1Q 506
-ip address 10.1.10.1 255.255.254.0
+ip address 10.3.10.1 255.255.254.0
 exit
 
 ip dhcp pool pool500
@@ -101,43 +101,43 @@ default-router 192.168.0.2
 exit
 
 ip dhcp pool pool501
-network 10.1.0.0 255.255.254.0
-default-router 10.1.0.1
+network 10.3.0.0 255.255.254.0
+default-router 10.3.0.1
 exit
 
 ip dhcp pool pool502
-network 10.1.2.0 255.255.254.0
-default-router 10.1.2.1
+network 10.3.2.0 255.255.254.0
+default-router 10.3.2.1
 exit
 
 ip dhcp pool pool503
-network 10.1.4.0 255.255.254.0
-default-router 10.1.4.1
+network 10.3.4.0 255.255.254.0
+default-router 10.3.4.1
 exit
 
 ip dhcp pool pool504
-network 10.1.6.0 255.255.254.0
-default-router 10.1.6.1
+network 10.3.6.0 255.255.254.0
+default-router 10.3.6.1
 exit
 
 ip dhcp pool pool505
-network 10.1.8.0 255.255.254.0
-default-router 10.1.8.1
+network 10.3.8.0 255.255.254.0
+default-router 10.3.8.1
 exit
 
 ip dhcp pool pool506
-network 10.1.10.0 255.255.254.0
-default-router 10.1.10.1
+network 10.3.10.0 255.255.254.0
+default-router 10.3.10.1
 exit
 
 ip dhcp excluded-address 192.168.0.1
 ip dhcp excluded-address 192.168.0.2
-ip dhcp excluded-address 10.1.0.1
-ip dhcp excluded-address 10.1.2.1
-ip dhcp excluded-address 10.1.4.1
-ip dhcp excluded-address 10.1.6.1
-ip dhcp excluded-address 10.1.8.1
-ip dhcp excluded-address 10.1.10.1
+ip dhcp excluded-address 10.3.0.1
+ip dhcp excluded-address 10.3.2.1
+ip dhcp excluded-address 10.3.4.1
+ip dhcp excluded-address 10.3.6.1
+ip dhcp excluded-address 10.3.8.1
+ip dhcp excluded-address 10.3.10.1
 
 
 enable
@@ -145,18 +145,18 @@ config term
 
 interface FastEthernet 0/0.507
 encapsulation dot1Q 507
-ip address 10.1.12.1 255.255.254.0
+ip address 10.3.12.1 255.255.254.0
 exit
 
 interface FastEthernet 0/0.508
 encapsulation dot1Q 508
-ip address 10.1.14.1 255.255.254.0
+ip address 10.3.14.1 255.255.254.0
 exit
 
-ip dhcp excluded-address 10.1.12.1
-ip dhcp excluded-address 10.1.12.2
-ip dhcp excluded-address 10.1.14.1
-ip dhcp excluded-address 10.1.14.2
+ip dhcp excluded-address 10.3.12.1
+ip dhcp excluded-address 10.3.12.2
+ip dhcp excluded-address 10.3.14.1
+ip dhcp excluded-address 10.3.14.2
 
 exit
 copy running-config startup-config
