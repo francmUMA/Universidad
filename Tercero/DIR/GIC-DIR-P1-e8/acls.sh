f72035222b4b4b5ee2df4fc8c7d2562599a277a1
francm9 <francano368@gmail.com>
2023-05-18 21:45:02 +0200

    acl 1

--- Tercero/DIR/GIC-DIR-P1-e8/acls.sh
+++ Tercero/DIR/GIC-DIR-P1-e8/acls.sh
@@ -1,11 +1,29 @@
 # Acl del router de la oficina
-# Vlan 400-in -- Todos los pcs pueden acceder al dns y al servidor web 
+
+enable
+
+conf t
+
+
+
+no ip access-list extended vlan400-in
 ip access-list extended vlan400-in
-permit udp any eq domain 10.3.12.0 0.0.1.255
-permit tcp any eq www 10.3.0.0 0.0.1.255 established
-permit tcp any eq www 10.3.6.0 0.0.1.255 established
+permit udp any 10.3.12.0 0.0.1.255 eq domain
+permit tcp any 10.3.0.0 0.0.1.255 eq www
+permit tcp any 10.3.6.0 0.0.1.255 eq www
+end
+write
+
 exit
 
 interface FastEthernet0/0.400
 ip access-group vlan400-in in
 exit
+
+no ip access-list extended vlanDNS-in
+ip access-list extended vlanDNS-in
+permit udp 10.3.12.0 0.0.1.255 eq domain 10.0.48.0 0.0.15.255
+permit tcp any 10.3.0.0 0.0.1.255 eq www
+permit tcp any 10.3.6.0 0.0.1.255 eq www
+end
+write
