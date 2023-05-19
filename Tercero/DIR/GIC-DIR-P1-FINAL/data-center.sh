64b26938dbbca19efc59f3fa470a056debb1d279
francm9 <francano368@gmail.com>
2023-05-19 17:32:50 +0200

    act

---
+++ Tercero/DIR/GIC-DIR-P1-FINAL/data-center.sh
@@ -0,0 +1,224 @@
+enable
+conf t
+
+interface FastEthernet 0/1
+switchport mode trunk
+exit
+
+interface FastEthernet 0/2
+switchport mode trunk
+exit
+
+interface FastEthernet 0/3
+switchport mode trunk
+exit
+
+interface FastEthernet 0/4
+switchport mode trunk
+exit
+
+exit
+copy running-config startup-config
+
+exit
+
+enable
+conf t
+
+vtp domain paco-data-center
+vtp mode client
+vtp password password
+vtp version 2
+
+exit 
+copy running-config startup-config
+
+exit
+
+enable
+config term
+
+spanning-tree vlan 504,505,506 root secondary
+spanning-tree vlan 500,501,502,503 root primary
+
+exit
+copy running-config startup-config
+
+exit
+
+enable
+config term
+
+spanning-tree mode rapid-pvst
+
+exit
+copy running-config startup-config
+
+exit
+
+interface FastEthernet 0/0
+no shutdown
+exit
+
+interface FastEthernet 1/0.500
+encapsulation dot1Q 500
+ip address 192.168.0.2 255.255.255.252
+exit
+
+interface FastEthernet 0/0.501
+encapsulation dot1Q 501
+ip address 10.3.0.1 255.255.254.0
+exit
+
+interface FastEthernet 0/0.502
+encapsulation dot1Q 502
+ip address 10.3.2.1 255.255.254.0
+exit
+
+interface FastEthernet 0/0.503
+encapsulation dot1Q 503
+ip address 10.3.4.1 255.255.254.0
+exit
+
+interface FastEthernet 0/0.504
+encapsulation dot1Q 504
+ip address 10.3.6.1 255.255.254.0
+exit
+
+interface FastEthernet 0/0.505
+encapsulation dot1Q 505
+ip address 10.3.8.1 255.255.254.0
+exit
+
+interface FastEthernet 0/0.506
+encapsulation dot1Q 506
+ip address 10.3.10.1 255.255.254.0
+exit
+
+ip dhcp pool pool500
+network 192.168.0.0 255.255.255.252
+default-router 192.168.0.2
+exit
+
+ip dhcp pool pool501
+network 10.3.0.0 255.255.254.0
+default-router 10.3.0.1
+exit
+
+ip dhcp pool pool502
+network 10.3.2.0 255.255.254.0
+default-router 10.3.2.1
+exit
+
+ip dhcp pool pool503
+network 10.3.4.0 255.255.254.0
+default-router 10.3.4.1
+exit
+
+ip dhcp pool pool504
+network 10.3.6.0 255.255.254.0
+default-router 10.3.6.1
+exit
+
+ip dhcp pool pool505
+network 10.3.8.0 255.255.254.0
+default-router 10.3.8.1
+exit
+
+ip dhcp pool pool506
+network 10.3.10.0 255.255.254.0
+default-router 10.3.10.1
+exit
+
+ip dhcp excluded-address 192.168.0.1
+ip dhcp excluded-address 192.168.0.2
+ip dhcp excluded-address 10.3.0.1
+ip dhcp excluded-address 10.3.2.1
+ip dhcp excluded-address 10.3.4.1
+ip dhcp excluded-address 10.3.6.1
+ip dhcp excluded-address 10.3.8.1
+ip dhcp excluded-address 10.3.10.1
+
+
+enable
+config term
+
+interface FastEthernet 0/0.507
+encapsulation dot1Q 507
+ip address 10.3.12.1 255.255.254.0
+exit
+
+interface FastEthernet 0/0.508
+encapsulation dot1Q 508
+ip address 10.3.14.1 255.255.254.0
+exit
+
+ip dhcp excluded-address 10.3.12.1
+ip dhcp excluded-address 10.3.12.2
+ip dhcp excluded-address 10.3.14.1
+ip dhcp excluded-address 10.3.14.2
+
+exit
+copy running-config startup-config
+
+exit
+
+#Configuracion de la vlan 500
+enable
+configure terminal
+
+interface FastEthernet 1/0
+no shutdown
+exit
+
+interface FastEthernet 1/0.500
+encapsulation dot1Q 500
+ip address 192.168.0.2 255.255.255.252
+exit
+
+ip dhcp excluded-address 192.168.0.1
+ip dhcp excluded-address 192.168.0.2
+exit
+copy running-config startup-config
+
+exit
+
+#Configuracion de routing dinamico
+enable
+configure terminal
+
+router eigrp 314
+no auto-summary
+network 192.168.0.4 0.0.0.3
+network 192.168.0.0 0.0.0.3
+network 10.3.0.0 0.0.1.255
+network 10.3.2.0 0.0.1.255
+network 10.3.4.0 0.0.1.255
+network 10.3.6.0 0.0.1.255
+network 10.3.8.0 0.0.1.255
+network 10.3.10.0 0.0.1.255
+network 10.3.12.0 0.0.1.255
+network 10.3.14.0 0.0.1.255
+exit
+exit
+copy running-config startup-config
+
+exit
+
+#Configuracion del enlace con router wan
+enable
+configure terminal
+
+interface FastEthernet 2/0
+no shutdown
+exit
+
+interface FastEthernet 2/0.509
+encapsulation dot1Q 509
+ip address 192.168.0.5 255.255.255.252
+exit
+exit
+copy running-config startup-config
+
+exit
+
---
+++ Tercero/DIR/GIC-DIR-P1-FINAL/office-config.sh
@@ -0,0 +1,198 @@
+interface FastEthernet 0/0
+no shutdown
+exit
+
+interface FastEthernet 0/0.400
+encapsulation dot1Q 400
+ip address 10.0.48.1 255.255.252.0
+exit
+
+interface FastEthernet 0/0.401
+encapsulation dot1Q 401
+ip address 10.0.52.1 255.255.254.0
+exit
+
+interface FastEthernet 0/0.402
+encapsulation dot1Q 402
+ip address 10.0.54.1 255.255.254.0
+exit
+
+interface FastEthernet 0/0.403
+encapsulation dot1Q 403
+ip address 10.0.56.1 255.255.254.0
+exit
+
+interface FastEthernet 0/0.404
+encapsulation dot1Q 404
+ip address 10.0.58.1 255.255.254.0
+exit
+
+interface FastEthernet 0/0.405
+encapsulation dot1Q 405
+ip address 10.0.60.1 255.255.255.0
+exit
+
+interface FastEthernet 0/0.406
+encapsulation dot1Q 406
+ip address 10.0.61.1 255.255.255.0
+exit
+
+interface FastEthernet 0/0.407
+encapsulation dot1Q 407
+ip address 10.0.62.1 255.255.255.128	
+exit
+
+interface FastEthernet3/0
+no shutdown
+ip address 192.168.0.14 255.255.255.252
+exit
+
+
+ip dhcp pool pool400
+network 10.0.48.0 255.255.252.0
+default-router 10.0.48.1
+exit
+
+ip dhcp pool pool401
+network 10.0.52.0 255.255.254.0
+default-router 10.0.52.1
+exit
+
+ip dhcp pool pool402
+network 10.0.54.0 255.255.254.0
+default-router 10.0.54.1
+exit
+
+ip dhcp pool pool403
+network 10.0.56.0 255.255.254.0
+default-router 10.0.56.1
+exit
+
+ip dhcp pool pool404
+network 10.0.58.0 255.255.254.0
+default-router 10.0.58.1
+exit
+
+ip dhcp pool pool405
+network 10.0.60.0 255.255.255.0
+default-router 10.0.60.1
+exit
+
+ip dhcp pool pool406
+network 10.0.61.0 255.255.255.0
+default-router 10.0.61.1
+exit
+
+ip dhcp pool pool407
+network 10.0.62.0 255.255.255.128
+default-router 10.0.62.1
+exit
+
+ip dhcp excluded-address 10.0.48.1
+ip dhcp excluded-address 10.0.52.1
+ip dhcp excluded-address 10.0.54.1
+ip dhcp excluded-address 10.0.56.1
+ip dhcp excluded-address 10.0.58.1
+ip dhcp excluded-address 10.0.60.1
+ip dhcp excluded-address 10.0.61.1
+ip dhcp excluded-address 10.0.62.1
+
+#Configuracion de la vlan 500
+enable
+configure terminal
+
+interface FastEthernet 1/0
+no shutdown
+exit
+
+interface FastEthernet 1/0.500
+encapsulation dot1Q 500
+ip address 192.168.0.1 255.255.255.252
+exit
+
+ip dhcp excluded-address 192.168.0.1
+ip dhcp excluded-address 192.168.0.2
+exit
+copy running-config startup-config
+
+exit
+
+#Configuracion de routing dinamico
+enable
+configure terminal
+
+router eigrp 314
+no auto-summary
+network 10.0.48.0 0.0.3.255
+network 10.0.52.0 0.0.1.255
+network 10.0.54.0 0.0.1.255
+network 10.0.56.0 0.0.1.255
+network 10.0.58.0 0.0.1.255
+network 10.0.60.0 0.0.0.255
+network 10.0.61.0 0.0.0.255
+network 10.0.62.0 0.0.0.127
+network 192.168.0.8 0.0.0.3
+network 192.168.0.0 0.0.0.3
+network 192.168.0.12 0.0.0.3   
+exit
+exit
+copy running-config startup-config
+
+exit
+
+#Configuracion del punto a punto con router wan
+enable
+configure terminal
+
+interface FastEthernet 2/0
+no shutdown
+exit
+
+interface FastEthernet 2/0.510
+encapsulation dot1Q 510
+ip address 192.168.0.9 255.255.255.252
+exit
+exit
+copy running-config startup-config
+
+exit
+
+# Configuracion de servidor dns
+enable
+configure terminal
+
+ip dhcp pool pool400
+dns-server 10.3.12.2
+exit
+
+ip dhcp pool pool401
+dns-server 10.3.12.2
+exit
+
+ip dhcp pool pool402
+dns-server 10.3.12.2
+exit
+
+ip dhcp pool pool403
+dns-server 10.3.12.2
+exit
+
+ip dhcp pool pool404
+dns-server 10.3.12.2
+exit
+
+ip dhcp pool pool405
+dns-server 10.3.12.2
+exit
+
+ip dhcp pool pool406
+dns-server 10.3.12.2
+exit
+
+ip dhcp pool pool407
+dns-server 10.3.12.2
+exit
+
+
+
+
