7d9d1e64416069660b6206be43f0e229199a6fbb
francm9 <francano368@gmail.com>
2023-09-26 11:57:24 +0200

    relacion matlab hecha

--- Cuarto/Control por Computador/Repaso Matlab y Simulink/chernobyl.m
+++ Cuarto/Control por Computador/Repaso Matlab y Simulink/chernobyl.m
@@ -7,9 +7,18 @@ k_y131 = log(2)/8*segundospordia;
 
 figure(1);
 hold on;
-subplot(2,1,1);
+subplot(3,1,1);
 plot(out.tout, out.media_c137, 'g');
 plot(out.tout, out.media_y131);
 title("Evolución de la cantidad por año");
 xlabel("Años"), ylabel("Cantidad (Kg)");
 hold off;
+subplot(3,1,2);
+plot(out.tout, out.media_c137);
+title("Evolución de la cantidad de Cesio-137 por año");
+xlabel("Años"), ylabel("Cantidad (Kg)");
+subplot(3,1,3);
+plot(out.tout, out.media_y131);
+title("Evolución de la cantidad de Yodo-131 por año");
+xlabel("Años"), ylabel("Cantidad (Kg)");
+
---
+++ Cuarto/Control por Computador/Repaso Matlab y Simulink/poblacion.m
@@ -0,0 +1,8 @@
+ L = 500;
+ K = 0.0006;
+ initial_condition = 10;
+
+ plot(out.tout,out.poblacion);
+ title("Evolución de la población de individuos");
+ xlabel("Tiempo"); ylabel("Cantidad de Individuos");
+ 
