61f43dc88f80723346fd5f5d58b64bb5616c044c
francm9 <francano368@gmail.com>
2023-10-07 14:08:33 +0200

    Update 01-heat-conduction.cu

--- Cuarto/Arquitecturas Paralelas/CUDA/Modulo 1/01-heat-conduction.cu
+++ Cuarto/Arquitecturas Paralelas/CUDA/Modulo 1/01-heat-conduction.cu
@@ -4,10 +4,34 @@
 // Simple define to index into a 1D array from 2D space
 #define I2D(num, c, r) ((r)*(num)+(c))
 
-/*
- * `step_kernel_mod` is currently a direct copy of the CPU reference solution
- * `step_kernel_ref` below. Accelerate it to run as a CUDA kernel.
- */
+__global__
+void step_kernel_mod(int ni, int nj, float fact, float* temp_in, float* temp_out)
+{
+  int i00, im10, ip10, i0m1, i0p1;
+  float d2tdx2, d2tdy2;
+
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  int i = idx % ni;
+  int j = idx / ni;
+
+  // loop over all points in domain (except boundary)
+  if (j > 0 && i > 0 && j < nj-1 && i < ni-1) {
+    // find indices into linear memory
+    // for central point and neighbours
+    i00 = I2D(ni, i, j);
+    im10 = I2D(ni, i-1, j);
+    ip10 = I2D(ni, i+1, j);
+    i0m1 = I2D(ni, i, j-1);
+    i0p1 = I2D(ni, i, j+1);
+
+    // evaluate derivatives
+    d2tdx2 = temp_in[im10]-2*temp_in[i00]+temp_in[ip10];
+    d2tdy2 = temp_in[i0m1]-2*temp_in[i00]+temp_in[i0p1];
+
+    // update temperatures
+    temp_out[i00] = temp_in[i00]+fact*(d2tdx2 + d2tdy2);
+  }
+}
 
 void step_kernel_ref(int ni, int nj, float fact, float* temp_in, float* temp_out)
 {
@@ -34,35 +58,6 @@ void step_kernel_ref(int ni, int nj, float fact, float* temp_in, float* temp_out
       temp_out[i00] = temp_in[i00]+fact*(d2tdx2 + d2tdy2);
     }
   }
-}
-
-__global__ void step_kernel_mod(int ni, int nj, float fact, float* temp_in, float* temp_out)
-{
-  int i00, im10, ip10, i0m1, i0p1;
-  float d2tdx2, d2tdy2;
-
-
-  // loop over all points in domain (except boundary)
-      // find indices into linear memory
-      // for central point and neighbours
-      int idx = blockDim.x * blockIdx.x + threadIdx.x;
-      int i = idx % ni;
-      int j = idx / ni;
-      
-      if (i > 0 && j > 0 && i < (ni - 1) && j < (nj - 1)){
-          i00 = I2D(ni, i, j);
-          im10 = I2D(ni, i-1, j);
-          ip10 = I2D(ni, i+1, j);
-          i0m1 = I2D(ni, i, j-1);
-          i0p1 = I2D(ni, i, j+1);
-
-          // evaluate derivatives
-          d2tdx2 = temp_in[im10]-2*temp_in[i00]+temp_in[ip10];
-          d2tdy2 = temp_in[i0m1]-2*temp_in[i00]+temp_in[i0p1];
-
-          // update temperatures
-          temp_out[i00] = temp_in[i00]+fact*(d2tdx2 + d2tdy2);
-      }
 }
 
 int main()
@@ -99,11 +94,17 @@ int main()
     temp2_ref= temp_tmp;
   }
 
-  int threads = nj*ni;
-  
+  int threads = ni * nj;
+  cudaError_t ierrSync, ierrAsync;
+
   // Execute the modified version using same data
   for (istep=0; istep < nstep; istep++) {
-    step_kernel_mod<<<threads/256,256>>>(ni, nj, tfac, temp1, temp2);
+    step_kernel_mod<<< threads / 256 + 1, 256 >>>(ni, nj, tfac, temp1, temp2);
+
+    ierrSync = cudaGetLastError();
+    ierrAsync = cudaDeviceSynchronize(); // Wait for the GPU to finish
+    if (ierrSync != cudaSuccess) { printf("Sync error: %s\n", cudaGetErrorString(ierrSync)); }
+    if (ierrAsync != cudaSuccess) { printf("Async error: %s\n", cudaGetErrorString(ierrAsync)); }
 
     // swap the temperature pointers
     temp_tmp = temp1;
