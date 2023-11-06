bfebec91f63c8e18f2f4cad2ea07efb0ebf6764b
francmUMA <francano368@uma.es>
2023-11-06 19:14:08 +0100

    Update primer_orden.m

--- Cuarto/Control por Computador/practica7/primer_orden.m
+++ Cuarto/Control por Computador/practica7/primer_orden.m
@@ -1,4 +1,8 @@
-syms M B s F;
-A = [1 (M*s^2)/B];
-E = [F/B];
-I = A \ E;
+M = 200;
+B = 10;
+F = 1.5;
+num = 1/M;
+den = [1 B/M];
+tf_pol = tf(num,den);
+tf_num = zpk(tf_pol);
+ltiview('step',F*tf_pol);
