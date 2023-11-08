4c43532b1a464bbfb3ec9134cbcd14b39f10b238
francmUMA <francano368@uma.es>
2023-11-08 10:35:08 +0100

    Update segundo_orden.m

--- Cuarto/Control por Computador/practica8/segundo_orden.m
+++ Cuarto/Control por Computador/practica8/segundo_orden.m
@@ -1,7 +1,37 @@
-M = 100000;
-m = 100;
-B = 100000;
-K = 100000;
+M = 100*1000;
+m = 1;
+K = 4*(pi^2)*M;
+B = 4*pi*M;
+F = m*9.8;
+num = 1/M;
+den = [1 B/M K/M];
+sis = tf(num, den);
+step(F*sis);
+%% Apartado D
+M = 100*1000;
+m = 1;
+K = 4*(pi^2)*M;
+B = 2*pi*M;
+F = m*9.8;
+num = 1/M;
+den = [1 B/M K/M];
+sis = tf(num, den);
+step(F*sis);
+%% Apartado E
+M = 100*1000;
+m = 1;
+K = 4*(pi^2)*M;
+B = 0;
+F = m*9.8;
+num = 1/M;
+den = [1 B/M K/M];
+sis = tf(num, den);
+step(F*sis);
+%% Apartado F
+M = 100*1000;
+m = 200*1000;
+K = 4*(pi^2)*M;
+B = 0;
 F = m*9.8;
 num = 1/M;
 den = [1 B/M K/M];
