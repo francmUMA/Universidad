c7ba0058298397b839ed2113ae33b82a9ee17b6e
Fco. Javier Cano Moreno <francano368@uma.es>
2023-12-01 19:12:19 +0100

    ej4 avance

--- Cuarto/ProgramaciónDistribuida/mpi/ej4.cpp
+++ Cuarto/ProgramaciónDistribuida/mpi/ej4.cpp
@@ -1,9 +1,16 @@
+//DEBE COMPILARSE CON mpic++ <nombre.cpp> -o <nombre_ejecutable>
+
 #include "/opt/homebrew/include/mpi.h"
 #include <cstdio>
-#include <string.h>
+#include <cstring>
+#include <string>
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 //#include <mpi.h>
+
+using namespace std;
 
 int main(int argc, char *argv[]){
     int rank, size;
@@ -11,27 +18,53 @@ int main(int argc, char *argv[]){
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD,&size);
 
+    char *msg = (char *)malloc(13*sizeof(char));
+    strcpy(msg, "HelloMPIWorld");
+    int subsize = strlen(msg) / (size-1);
+
+
     if (rank == 0){
-        char *msg = (char*)malloc(13);
-        strcpy(msg, "HelloMPIWorld");
-    
-        //Envir a cada proceso un trozo de la cadena
-        char *submsg = (char*)malloc(13 / size + 1);
-        printf("Size msg: %lu, Size submsg: %lu\n", sizeof(msg),sizeof(msg) / size + 1);
-        for (int i = 1; i < size; i++){
-            //Crear subcadena
-            strcpy(submsg, "");
-            for (int j = (i-1)*(sizeof(msg)/size); j < (sizeof(msg) / size) * i; j++){
-                printf("j: %d, m[j]: %c\n", j, msg[j]);
-                strcat(submsg, &msg[j]); 
-            }
-            printf("Proceso %d: %s\n", i, submsg);
-            //MPI_Send(submsg, sizeof(submsg), MPI_CHAR, i, 0, MPI_COMM_WORLD);
-        }}
-/*    } else {*/
-        /*char * msg = (char*)malloc(sizeof(msg) / size + 1);*/
-        /*MPI_Recv(&msg, 13, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);*/
-        /*printf("Proceso %d: %s\n", rank, msg);*/
-    /*}*/
+        //Crear subcadenas de igual tamaño para cada proceso
+        for (int i = 1; i < size-1; i++){
+            char *submsg = (char *)malloc(subsize*sizeof(char));
+            strncpy(submsg, msg+(i-1)*subsize, subsize);
+            printf("El proceso maestro envía a %d: %s\n", i, submsg);
+            MPI_Send(submsg, strlen(submsg), MPI_CHAR, i, 0, MPI_COMM_WORLD);
+        }
+        //El último proceso recibe el resto de la cadena
+        char *submsg = (char *)malloc(strlen(msg) - (size-2)*subsize);
+        strncpy(submsg, msg+(size-2)*subsize, strlen(msg) - (size-2)*subsize);
+        printf("El proceso maestro envía a %d: %s\n", size-1, submsg);
+        MPI_Send(submsg, strlen(submsg), MPI_CHAR, size-1, 0, MPI_COMM_WORLD);
+
+        //Recibir los mensajes de los procesos
+        /*string subres;*/
+        /*string res;*/
+        /*for (int i = 1; i < size; i++){*/
+            /*MPI_Recv(&subres, 13, MPI_CHAR, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);*/
+            /*printf("Proceso master recibe de %d: %s\n", i, subres.c_str());*/
+            /*res += subres;*/
+        /*}*/
+        /*printf("Cadena original: %s\n", msg.c_str());*/
+        /*printf("Cadena invertida: %s\n", res.c_str());*/
+    } else if (rank > 0 && rank < size-1){
+        char *submsg = (char *)malloc(subsize*sizeof(char));
+        MPI_Recv(&submsg, subsize, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("El proceso %d recibe: %s\n", rank, submsg);
+
+        //Invertir la subcadena
+
+        printf("El proceso %d envía: %s\n", rank, submsg);
+        MPI_Send(submsg, strlen(submsg), MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+    } else if (rank == size-1){
+        char *submsg = (char *)malloc((strlen(msg) - (size-2)*subsize)*sizeof(char));
+        MPI_Recv(submsg, (strlen(msg) - (size-2)*subsize)*sizeof(char), MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("El proceso %d recibe: %s\n", rank, submsg);
+
+        //Invertir la subcadena
+        
+        printf("El proceso %d envía: %s\n", rank, submsg);
+        MPI_Send(submsg, strlen(submsg), MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+    }
     MPI_Finalize();
 }
